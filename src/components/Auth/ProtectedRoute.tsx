487e66b95d48be53fce157f6a725d6695a3b7e10
ncurnier <nicolascurnier@gmail.com>
2025-07-11 18:19:25 +0200

    Updated ProtectedRoute.tsx

--- src/components/Auth/ProtectedRoute.tsx
+++ src/components/Auth/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
