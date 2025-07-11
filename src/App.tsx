ccc306ec369bb01831057b0cb6a7ee90aab9acc9
ncurnier <nicolascurnier@gmail.com>
2025-07-11 17:20:41 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,28 @@ import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
 import AdminPanel from './pages/AdminPanel.tsx';
 import { ProtectedRoute } from './components/Auth/ProtectedRoute.tsx';
-import { AuthProvider } from './contexts/AuthContext.tsx';
-import { CartProvider } from './contexts/CartContext.tsx';
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/oeuvres" element={<Artists />} />
-            <Route path="/oeuvre/:id" element={<ArtworkDetail />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            {/* Admin only */}
-            <Route 
-              path="/admin/*" 
-              element={
-                <ProtectedRoute role="admin">
-                  <AdminPanel />
-                </ProtectedRoute>
-              } 
-            />
-          </Routes>
-        </Layout>
-      </CartProvider>
-    </AuthProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/oeuvres" element={<Artists />} />
+        <Route path="/oeuvre/:id" element={<ArtworkDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Admin only */}
+        <Route 
+          path="/admin/*" 
+          element={
+            <ProtectedRoute role="admin">
+              <AdminPanel />
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
+    </Layout>
   );
 }
 
