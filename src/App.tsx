cdf7cf93b1946b82f5c9b798c484b4e884689545
ncurnier <nicolascurnier@gmail.com>
2025-07-11 17:15:52 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,36 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { CartProvider } from './contexts/CartContext';
-import Header from './components/Layout/Header';
-import Footer from './components/Layout/Footer';
-import ProtectedRoute from './components/Auth/ProtectedRoute';
-
-// Pages
-import Home from './pages/Home';
-import Gallery from './pages/Gallery';
-import Artists from './pages/Artists';
-import Subscriptions from './pages/Subscriptions';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import AdminPanel from './pages/AdminPanel';
-import ArtworkDetail from './pages/ArtworkDetail';
-import Checkout from './pages/Checkout';
-import Support from './pages/Support';
-import NotFound from './pages/NotFound';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home.tsx';
+import Artists from './pages/Artists.tsx';
+import ArtworkDetail from './pages/ArtworkDetail.tsx';
+import Checkout from './pages/Checkout.tsx';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+import AdminPanel from './pages/AdminPanel.tsx';
+import { ProtectedRoute } from './components/Auth/ProtectedRoute.tsx';
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <div className="min-h-screen bg-gray-50">
-          <Header />
-          <main className="min-h-screen">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/artwork/:id" element={<ArtworkDetail />} />
-              <Route path="/artists" element={<Artists />} />
-              <Route path="/subscriptions" element={<Subscriptions />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/support" element={<Support />} />
-              
-              {/* Protected Routes */}
-              <Route path="/profile" element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              } />
-              <Route path="/checkout" element={
-                <ProtectedRoute>
-                  <Checkout />
-                </ProtectedRoute>
-              } />
-              <Route path="/admin" element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminPanel />
-                </ProtectedRoute>
-              } />
-              
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </CartProvider>
-    </AuthProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/oeuvres" element={<Artists />} />
+        <Route path="/oeuvre/:id" element={<ArtworkDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Routes protégées pour admin */}
+        <Route 
+          path="/admin/*" 
+          element={
+            <ProtectedRoute role="admin">
+              <AdminPanel />
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
+    </Layout>
   );
 }
 
